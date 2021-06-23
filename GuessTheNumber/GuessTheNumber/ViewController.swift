cd0f4e13ce4142708eca133360b559aee3fe7d81
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2021-06-23 09:58:21 +0300

    Alerts and update state were added. Logic was implemented.

--- GuessTheNumber/GuessTheNumber/ViewController.swift
+++ GuessTheNumber/GuessTheNumber/ViewController.swift
@@ -17,21 +17,39 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        sliderLabel.value = 50
+        updateState(action: nil)
     }
-
-    func guessTheNumber() -> Int {
+    
+    func guessTheNumber() {
         randomNumber = Int.random(in: 0...100)
-        return randomNumber
+        print(randomNumber)
+    }
+    
+    func updateState(action: UIAlertAction!) {
+        sliderLabel.value = 50
+        valueLabel.text = ""
+        guessTheNumber()
+    }
+    
+    func alertGeenrate(alertTitle: String, alertMessage: String, actionTitle: String, handler: ((UIAlertAction) -> Void)? = nil) {
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: actionTitle, style: .default, handler: handler))
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
         valueLabel.text = String(format: "%.f", sender.value)
+        answer = Int(round(sender.value))
     }
     
     @IBAction func checkAnswerTapped(_ sender: UIButton) {
+        if answer < randomNumber {
+            alertGeenrate(alertTitle: "Wrong", alertMessage: "Your answer \(answer) is less than the guessed number.", actionTitle: "Try again", handler: nil)
+        } else if answer > randomNumber {
+            alertGeenrate(alertTitle: "Wrong", alertMessage: "Your answer \(answer) is more than the guessed number.", actionTitle: "Try again", handler: nil)
+        } else {
+            alertGeenrate(alertTitle: "Right", alertMessage: "Your have guessed the number. Your answer \(answer) equals the guessed number. Сongratulations!", actionTitle: "OK", handler: updateState)
+        }
     }
-    
-    
 }
 
