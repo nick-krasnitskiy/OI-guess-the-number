e1fdff4c3cd89dc0b5ec746fc049159fd6abf538
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2021-06-23 08:20:55 +0300

    Slider label outlet  and necessary variables were added. Changing of value label was implemented.

--- GuessTheNumber/GuessTheNumber/ViewController.swift
+++ GuessTheNumber/GuessTheNumber/ViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var sliderLabel: UISlider!
     
-
+    var randomNumber: Int = 0
+    var answer: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        sliderLabel.value = 50
     }
 
 
     @IBAction func sliderChanged(_ sender: UISlider) {
+        valueLabel.text = String(format: "%.f", sender.value)
     }
     
     @IBAction func checkAnswerTapped(_ sender: UIButton) {
