d23a734e26edbf698b49284138d8e63c60c3f6b2
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2021-06-28 09:34:50 +0300

    Add second screen UI localization.

--- GuessTheNumber/GuessTheNumber/SecondViewController.swift
+++ GuessTheNumber/GuessTheNumber/SecondViewController.swift
@@ -11,8 +11,15 @@ class SecondViewController: UIViewController {
     
     var delegate: ViewControllerDelegate?
     
+    @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var fromTextField: UITextField!
     @IBOutlet weak var toTextField: UITextField!
+    @IBOutlet weak var addButtonLabel: UIButton!
+    
+    
+    override func viewDidLoad() {
+        updateUI()
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
@@ -21,5 +28,12 @@ class SecondViewController: UIViewController {
     @IBAction func addRangePressed(_ sender: UIButton) {
         guard let fromBorder = fromTextField.text, let toBorder = toTextField.text else {return}
         delegate?.update(fromBorder: fromBorder, toBorder: toBorder)
+    }
+    
+    func updateUI() {
+        mainLabel.text = NSLocalizedString("SC_LABEL", comment:"Range of random number generation")
+        fromTextField.placeholder = NSLocalizedString("SC_PH_ONE", comment:"From")
+        toTextField.placeholder = NSLocalizedString("SC_PH_TWO", comment:"To")
+        addButtonLabel.setTitle(NSLocalizedString("SC_ADD_BUTTON", comment:"ADD"), for: .normal)
     }
 }
